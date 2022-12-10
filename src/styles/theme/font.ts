f58c39ae1e212bc91be6fcca43e5586bfd374341
Rintaro Itokawa <me@re-taro.dev>
2022-12-10 23:25:50 +0900

    chore: Format code

--- src/styles/theme/font.ts
+++ src/styles/theme/font.ts
@@ -1,13 +1,13 @@
-import {Kosugi_Maru} from "@next/font/google";
+import { Kosugi_Maru } from "@next/font/google";
 import type { DeepPartial, Theme } from "@chakra-ui/react";
 
 const KosugiMaru = Kosugi_Maru({
-    weight: "400",
-    subsets: ["japanese"],
-    display: "swap",
+  weight: "400",
+  subsets: ["japanese"],
+  display: "swap",
 });
 
 export const fonts: DeepPartial<Theme["fonts"]> = {
-    heading: `${KosugiMaru.style.fontFamily}, sans-serif`,
-    body: `${KosugiMaru.style.fontFamily}, sans-serif`,
+  heading: `${KosugiMaru.style.fontFamily}, sans-serif`,
+  body: `${KosugiMaru.style.fontFamily}, sans-serif`,
 };
