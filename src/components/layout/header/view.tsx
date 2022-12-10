9704e5683a64b2c41c052238076db6eedbe152b4
sansai-SNCT <syunzo1115@icloud.com>
2022-12-11 00:21:13 +0900

    feat: Support for long descriptions

--- src/components/layout/header/view.tsx
+++ src/components/layout/header/view.tsx
@@ -19,9 +19,25 @@ export const ViewHeader: FC<ViewHeaderProps> = ({ date, name, title }) => (
     py={1}
   >
     <Text fontSize="md">{date}</Text>
-    <Text fontSize="2xl" borderBottom="1px" borderColor="primary.500">
+    <Text
+      fontSize="2xl"
+      borderBottom="1px"
+      borderColor="primary.500"
+      maxWidth="90vw"
+      whiteSpace="nowrap"
+      overflow="hidden"
+      textOverflow="ellipsis"
+    >
       {name}
     </Text>
-    <Text fontSize="md">{title}</Text>
+    <Text
+      fontSize="md"
+      maxWidth="90vw"
+      whiteSpace="nowrap"
+      overflow="hidden"
+      textOverflow="ellipsis"
+    >
+      {title}
+    </Text>
   </Flex>
 );
