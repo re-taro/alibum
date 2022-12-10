2de4b58c8766012721d9a4963568a52ab1e112d6
Bayathy <misosoup0321@icloud.com>
2022-12-10 17:01:23 +0900

    format

--- src/libs/firebase/storage.ts
+++ src/libs/firebase/storage.ts
@@ -1,14 +1,18 @@
 import { ref, StorageReference, uploadBytes } from "firebase/storage";
 import { storage } from "./init";
 
-export const createStoregeRef = (filename: string, uuid: string) : StorageReference=>
-  ref(storage, `images/${uuid}/${filename}`);
+export const createStoregeRef = (
+  filename: string,
+  uuid: string,
+): StorageReference => ref(storage, `images/${uuid}/${filename}`);
 
-export const uploadImage = (image: File, uuid: string) : Promise<StorageReference> => {
-  const fileRef = uploadBytes(
-    createStoregeRef(image.name, uuid),
-    image,
-  ).then((snapshot) => snapshot.ref);
+export const uploadImage = (
+  image: File,
+  uuid: string,
+): Promise<StorageReference> => {
+  const fileRef = uploadBytes(createStoregeRef(image.name, uuid), image).then(
+    (snapshot) => snapshot.ref,
+  );
 
   return fileRef;
 };
