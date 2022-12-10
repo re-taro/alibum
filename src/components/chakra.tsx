3a13c68e959f42c3d16056143bc2ea808d300a2c
Bayathy <misosoup0321@icloud.com>
2022-12-10 17:35:40 +0900

    Revert "feat: Add reset css"
    This reverts commit e83dfeed443e2b5accfbcc22d58c711644e539a7.

--- src/components/chakra.tsx
+++ src/components/chakra.tsx
@@ -1,13 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import type { FC, ReactNode } from "react";
+import type { FC } from "react";
 import customTheme from "../styles/theme";
 
 type ChakraProps = {
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
 export const Chakra: FC<ChakraProps> = ({ children }) => (
-  <ChakraProvider theme={customTheme} resetCSS>
-    {children}
-  </ChakraProvider>
+  <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
 );
