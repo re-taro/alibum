a337185902e18a07839314f74b823ff7cfe0fb59
Bayathy <misosoup0321@icloud.com>
2022-12-11 15:40:24 +0900

    feat: add function type

--- src/hooks/use-store-list.ts
+++ src/hooks/use-store-list.ts
@@ -1,7 +1,10 @@
 import { getList, StoreList } from "libs/firebase/store";
 import { useState, useMemo } from "react";
+import type { Dispatch, SetStateAction } from "react";
 
-export const useStoreList = (uuid: string) => {
+export const useStoreList = (
+  uuid: string,
+): [StoreList, Dispatch<SetStateAction<StoreList>>, boolean] => {
   const [storeList, setListState] = useState<StoreList>([]);
   const [isLoading, setLodingState] = useState<boolean>(true);
   useMemo(() => {
