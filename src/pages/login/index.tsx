bab39ecd831d1f0794aaf0a5d54d4aa74e356c6c
Rintaro Itokawa <me@re-taro.dev>
2022-12-12 03:20:02 +0900

    feat: Create login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,26 +1,53 @@
-import { Box, Text } from "@chakra-ui/react";
-import type { HTMLChakraProps } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import type { FC } from "react";
+import { Flex, Text, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Logo } from "../../components/alibum";
+import { login } from "../../libs/firebase/auth";
+import { useAuthContext } from "../../contexts/auth";
 
-const GradientText: FC<HTMLChakraProps<"p">> = (chakraProps) => (
-  <Text
-    fontSize="6vw"
-    background="linear-gradient(90deg, #4d62d0, #d152c9 30%, #e6b357)"
-    backgroundClip="text"
-    {...chakraProps}
-  />
-);
+const Login: NextPage = () => {
+  const { user } = useAuthContext();
+  const router = useRouter();
+  if (user) {
+    router.push("/");
+  }
+  const onClick = (): void => {
+    login();
+  };
+  return (
+    <Flex
+      as="main"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minH="100vh"
+      bgColor="background.500"
+      px={{ base: "12", lg: "25%" }}
+    >
+      <Logo
+        width={{ base: "8.53125rem", md: "11.375rem", lg: "17.0625rem" }}
+        height={{ base: "4.6875rem", md: "6.25rem", lg: "9.375rem" }}
+        pb={{ base: "4", md: "9" }}
+      />
+      <Text
+        as="b"
+        fontSize={{ base: "xs", md: "xl" }}
+        pb={{ base: "28", md: "64" }}
+      >
+        毎日の感謝をサプライズしよう
+      </Text>
+      <Button
+        colorScheme="extra"
+        borderRadius="2xl"
+        px="4"
+        color="white"
+        fontSize="1.25rem"
+        onClick={onClick}
+      >
+        login
+      </Button>
+    </Flex>
+  );
+};
 
-const LoginPage: NextPage = () => (
-  <Box
-    height="100vh"
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <GradientText as="h2">Hello Chakra UI</GradientText>
-  </Box>
-);
-
-export default LoginPage;
+export default Login;
