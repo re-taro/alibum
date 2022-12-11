f5e83714ace54ab6b1ec25518fff9fc8b1f18079
Bayathy <misosoup0321@icloud.com>
2022-12-12 04:49:02 +0900

    fix: Add aspect-ratio at Image

--- src/components/card/edit.tsx
+++ src/components/card/edit.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { Card, CardBody, Flex, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, Card, CardBody, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
 type EditCardProps = {
@@ -13,18 +13,23 @@ export const EditCard: FC<EditCardProps> = ({ text, imageref }) =>
       shadow="md"
       rounded="md"
       width="70vw"
+      height="auto"
       maxWidth="30rem"
       backgroundColor="white"
     >
       <CardBody position="relative">
-        <Flex flexDir="column" alignItems="center" textAlign="center">
+        <Flex flexDir="column" alignItems="center" textAlign="center" gap={4}>
           <Text fontSize={{ base: "1rem", lg: "1.5rem" }}>{text}</Text>
-          <Image
-            src={imageref}
-            alt="Present for you"
-            width={128}
-            height={128}
-          />
+          <Box w="full">
+            <AspectRatio ratio={16 / 9}>
+              <Image
+                src={imageref}
+                alt="Present for you"
+                fill
+                style={{ objectFit: "contain" }}
+              />
+            </AspectRatio>
+          </Box>
         </Flex>
       </CardBody>
     </Card>
