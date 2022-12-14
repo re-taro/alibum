ffe9dd8badbafffcb4693fae558fe7ce6bfe8fa8
Rintaro Itokawa <me@re-taro.dev>
2022-12-15 02:33:42 +0900

    feat: Update SEO config

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -8,7 +8,12 @@ export const Meta = (): JSX.Element => (
     <meta name="apple-mobile-web-app-title" content={APP_NAME} />
     <meta name="format-detection" content="telephone=no" />
     <meta name="mobile-web-app-capable" content="yes" />
-    <meta name="theme-color" content="#FFFFFF" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="manifest" href="/manifest.json" />
   </>
