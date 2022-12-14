07ef39fb8c889c69e990be09b73216c0553ab0d9
Rintaro Itokawa <me@re-taro.dev>
2022-12-15 02:33:59 +0900

    deps: Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  swcMinify: true,
-  reactStrictMode: true,
-  images: {
-    domains: ["firebasestorage.googleapis.com"],
-  },
-  eslint: {
-    dirs: ["src"],
-  },
-});
+module.exports = withBundleAnalyzer(
+  withPWA({
+    swcMinify: true,
+    reactStrictMode: true,
+    images: {
+      domains: ["firebasestorage.googleapis.com"],
+    },
+    eslint: {
+      dirs: ["src"],
+    },
+  }),
+);
