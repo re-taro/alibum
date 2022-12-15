7c00f71c3d369d0057e1b20953669a1ad1a80d57
Bayathy <misosoup0321@icloud.com>
2022-12-16 05:51:43 +0900

    feat: Change Dynamic Routing

--- src/pages/view/[uuid].tsx
+++ src/pages/view/[uuid].tsx
@@ -1,7 +1,5 @@
-import { Box, Text } from "@chakra-ui/react";
-import type { HTMLChakraProps } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import type { NextPageWithLayout } from "next";
-import type { FC } from "react";
 import { createGetLayout } from "../../components/layout/view";
 import type { ViewLayoutProps } from "../../components/layout/view";
 
@@ -11,15 +9,6 @@ const data: ViewLayoutProps = {
   date: "4/25",
 };
 
-const GradientText: FC<HTMLChakraProps<"p">> = (chakraProps) => (
-  <Text
-    fontSize="6vw"
-    background="linear-gradient(90deg, #4d62d0, #d152c9 30%, #e6b357)"
-    backgroundClip="text"
-    {...chakraProps}
-  />
-);
-
 const View: NextPageWithLayout = () => (
   <Box
     height="100vh"
@@ -27,7 +16,7 @@ const View: NextPageWithLayout = () => (
     justifyContent="center"
     alignItems="center"
   >
-    <GradientText as="h2">Hello Chakra UI</GradientText>
+    
   </Box>
 );
 
