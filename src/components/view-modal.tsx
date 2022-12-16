bfa36f8c95d5690c33aac74149e597f9dce59ba5
Bayathy <misosoup0321@icloud.com>
2022-12-17 02:04:44 +0900

    feat: Restyle AspectRatio

--- src/components/view-modal.tsx
+++ src/components/view-modal.tsx
@@ -64,7 +64,7 @@ export const ViewModal: FC<ViewModalProps> = ({
 
           {imageRef ? (
             <Box w="full">
-              <AspectRatio ratio={16 / 9}>
+              <AspectRatio ratio={1 / 1}>
                 <Image
                   src={imageRef}
                   alt="Present for you"
